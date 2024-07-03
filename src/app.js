a7e1487926a10e2887c22a5d00126d0dd26539ca
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-03 07:02:27 -0700

    added file iteration and example.js framework

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const { file } = require('googleapis/build/src/apis/file');
 require('dotenv').config();
 
 const port = process.env.PORT;
+const configPath = path.join(__dirname, 'configs');
+
+const 
 
 const corsOptions = {
     origin: process.env.CORS_ORIGIN,
@@ -17,6 +23,30 @@ checkSheetCode() //make this function
 setInterval(() => {
     // Add check sheet code here
 }, 1000 * 60 * 5);
+
+app.get('/:configName', async (req, res) => {
+    fs.readdir(configPath, (err, files) => {
+
+        if (err) {
+            res.status(500).send('Internal server error');
+            return;
+        }
+    
+        const jsFiles = files.filter(file => file.endsWith('.js'));
+
+        if (!files.includes(`${req.params.configName}.js`)) {
+            res.status(404).send('Config not found');
+            return;
+        } 
+
+        jsFiles.forEach(file => {
+            const filePath = path.join(configPath, file);
+            const module = require(filePath);
+
+            module.main();
+        });
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ src/configs/example.js
@@ -0,0 +1,15 @@
+
+// You can find this in the URL of your Google Sheet
+//docs.google.com/spreadsheets/d/(THE SPREADSHEET ID)/edit?
+const spreadSheetID = '1UodW6GUWXVxuCnBtKwYelX8Iu_S6ush2NuY4QPFsRkY'
+
+export default function main () {
+    googleSheets.getSpreadSheetValues({
+        spreadsheetId: spreadSheetID,
+        sheetName: 'New All Data'
+    }).then((res) => {
+        console.log(res.data.values);
+    });
+
+    return;
+}
